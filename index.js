1590eac41a543da06c4862d407b81878547fdde3
sdshuvo98 <hellosdshuvo@gmail.com>
2022-08-03 18:54:34 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bqi0w.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("database").collection("users");
+
+        console.log('MongoDB connected')
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send("Hello from Mikhvision!")
